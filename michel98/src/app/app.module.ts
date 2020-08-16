6fb76dd43bf6014cd3bdf779f61de474164bf259
michele antonacci <michele.antonacci@linksmt.it>
2020-08-16 20:11:26 +0200

    Header e Sidebar

--- michel98/src/app/app.module.ts
+++ michel98/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { EsperienzeComponent } from './esperienze/esperienze.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    FooterComponent,
+    AboutComponent,
+    HeaderComponent,
+    EsperienzeComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
---
+++ michel98/src/app/esperienze/esperienze.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-esperienze',
+  templateUrl: './esperienze.component.html',
+  styleUrls: ['./esperienze.component.css']
+})
+export class EsperienzeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
