4c62f1e41f99deb1b1b4a3256756d479c4004abc
michele antonacci <michele.antonacci@linksmt.it>
2020-08-20 09:44:44 +0200

    fine prova dx

--- docs/assets/js/scripts.js
+++
@@ -1,6 +0,0 @@
-function checkAnimations() {
-    var inputs = document.getElementsByTagName('input');
-    inputs.forEach(function (entry) {
-    });
-}
-window.__PUBLIC_PATH__ = './assets/bootstrap-italia/dist/fonts';
