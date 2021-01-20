7569c4148931f80d0749599ad635267b555c78d9
michele antonacci <michele.antonacci@dxc.com>
2021-01-20 22:30:05 +0100

    Refactoring

--- michel98/src/app/app.module.ts
+++ michel98/src/app/app.module.ts
@@ -1,24 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { FooterComponent } from './footer/footer.component';
-import { AboutComponent } from './about/about.component';
-import { HeaderComponent } from './header/header.component';
-import { EsperienzeComponent } from './esperienze/esperienze.component';
 import { ContentComponent } from './content/content.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    FooterComponent,
-    AboutComponent,
-    HeaderComponent,
-    EsperienzeComponent,
-    ContentComponent
+    ContentComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
